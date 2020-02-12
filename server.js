bd63f2982b59b34f376a7dcd73c496b9d377614e
Christina Gerard <ChristinaGerard@Christinas-MBP.hsd1.ga.comcast.net>
2020-02-12 18:31:43 -0500

    moved some folders around

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
 const log = console.log;
+
+// packages
+
+// dotenv
 const dot = require('dotenv').config();
+
+// express
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+// path
+const path = require('path');
+
+// API KEY
 const API_KEY = dot.parsed.API_KEY;
 
-// log(API_KEY);
 
+
+app.get('/', (req, res) =>{
+    res.send('Hello!');
+})
+
+app.listen(PORT, err => {
+    err ? log(err) : log(`http://localhost:${PORT}`);
+})
 
 
 
