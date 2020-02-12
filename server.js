9e166d947a454525b6cd38a2452833c88968dc30
Christina Gerard <ChristinaGerard@Christinas-MBP.hsd1.ga.comcast.net>
2020-02-12 18:52:18 -0500

    added middleware and sending index file to UI

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const dot = require('dotenv').config();
 // express
 const express = require('express');
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 3000;
 
 // path
 const path = require('path');
@@ -16,11 +16,23 @@ const path = require('path');
 // API KEY
 const API_KEY = dot.parsed.API_KEY;
 
+// Middleware
+app.use(express.static(__dirname + '/public/assets'));
+app.use('/', express.static(path.join(__dirname + '/node_modules')));
+
+// app.get('/', (req, res) =>{
+//     // res.send('Hello!');
+//     res.sendFile(path.dirname('public'));
+// })
 
 
-app.get('/', (req, res) =>{
-    res.send('Hello!');
-})
+app.route('/')
+    .post((req, res) =>{
+        res.send({API_KEY})
+    })
+    .get((req, res) =>{
+        res.sendFile(path.join(__dirname, "/public/index.html"));
+    });
 
 app.listen(PORT, err => {
     err ? log(err) : log(`http://localhost:${PORT}`);
